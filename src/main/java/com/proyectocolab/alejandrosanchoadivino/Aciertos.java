7863d9ed7398245797afdb57465f129cf04f6a81
AlejandroSancho <>
2020-11-17 16:26:56 +0100

    Modificación final.

--- src/main/java/com/proyectocolab/alejandrosanchoadivino/Aciertos.java
+++ src/main/java/com/proyectocolab/alejandrosanchoadivino/Aciertos.java
@@ -6,7 +6,7 @@ public class Aciertos {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        //Limitando el valor random a entre 10 y 50.
+        //Limitando el valor random a entre 10 y 50
         int x = (int)((Math.random()*(50-10))+10);
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
