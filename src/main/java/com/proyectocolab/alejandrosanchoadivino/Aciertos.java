13cc4314b612a6b121b31cf96e90009f7a4d26d2
AlejandroSancho <34617@DESKTOP-QUD65H0>
2020-11-17 16:26:34 +0100

    Modificación final.

--- src/main/java/com/proyectocolab/alejandrosanchoadivino/Aciertos.java
+++ src/main/java/com/proyectocolab/alejandrosanchoadivino/Aciertos.java
@@ -6,16 +6,17 @@ public class Aciertos {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        //Limitando el valor random a entre 10 y 50.
+        int x = (int)((Math.random()*(50-10))+10);
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
-            System.out.print("\nIntroduce el valor de X: ");
+            System.out.println("Introduce el valor de X: ");
             n = sc.nextInt();
             if (n > x) {
-                System.out.print("Te pasaste Amigo");
+                System.out.println("Te pasaste Amigo");
             } else if (n < x){
-                System.out.print("Casi cerca ...");
+                System.out.println("Casi cerca ...");
             }
             cont++;
         } while (n != x);
